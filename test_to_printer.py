d9346c5d9ce45eaece14294ef4d68d5550cacfeb
Αpostolos-Valiakos <45800958+Apostolos-Valiakos@users.noreply.github.com>
2023-01-20 22:57:23 +0200

    Windows python print test

--- test_to_printer.py
+++ test_to_printer.py
@@ -6,25 +6,31 @@
 
 
 import os, sys
-import win32print
-printer_name = win32print.GetDefaultPrinter ()
-#
-# raw_data could equally be raw PCL/PS read from
-#  some print-to-file operation
-#
-if sys.version_info >= (3,):
-  raw_data = bytes ("This is a test", "utf-8")
-else:
-  raw_data = "This is a test"
+from win32 import win32print
+# p = win32print.GetDefaultPrinter()
+p = win32print.OpenPrinter("PDFCreator")
 
-hPrinter = win32print.OpenPrinter (printer_name)
-try:
-  hJob = win32print.StartDocPrinter (hPrinter, 1, ("test of raw data", None, "RAW"))
-  try:
-    win32print.StartPagePrinter (hPrinter)
-    win32print.WritePrinter (hPrinter, raw_data)
-    win32print.EndPagePrinter (hPrinter)
-  finally:
-    win32print.EndDocPrinter (hPrinter)
-finally:
-  win32print.ClosePrinter (hPrinter)
+
+
+job = win32print.StartDocPrinter(p, 1, ("data", None, "RAW"))
+win32print.StartPagePrinter(p)
+data = "your_data_here"
+data = bytes(data, 'utf-8')
+win32print.WritePrinter (p, data)
+win32print.EndPagePrinter (p)
+# if sys.version_info >= (3,):
+#   raw_data = bytes ("This is a test", "utf-8")
+# else:
+#   raw_data = "This is a test"
+
+# hPrinter = win32print.OpenPrinter (printer_name)
+# try:
+#   hJob = win32print.StartDocPrinter (hPrinter, 1, ("test of raw data", None, "RAW"))
+#   try:
+#     win32print.StartPagePrinter (hPrinter)
+#     win32print.WritePrinter (hPrinter, raw_data)
+#     win32print.EndPagePrinter (hPrinter)
+#   finally:
+#     win32print.EndDocPrinter (hPrinter)
+# finally:
+#   win32print.ClosePrinter (hPrinter)
